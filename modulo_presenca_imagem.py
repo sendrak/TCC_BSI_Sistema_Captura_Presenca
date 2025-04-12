6c08e87c0d5b70b0ca485ddb60178e61cf5bf876
Lucas de Figueiredo Aguiar <37455976+sendrak@users.noreply.github.com>
2025-04-12 17:44:14 -0300

    Update Presença por Imagem
    Seleção de disciplina/diretório dinâmicos

--- modulo_presenca_imagem.py
+++ modulo_presenca_imagem.py
@@ -6,6 +6,7 @@ import numpy as np
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
+from kivy.uix.dropdown import DropDown
 from kivy.uix.image import Image
 from kivy.uix.filechooser import FileChooserListView
 from kivy.uix.popup import Popup
@@ -14,6 +15,9 @@ from kivy.uix.textinput import TextInput
 from openpyxl import Workbook
 
 import face_recognition
+
+from helper_funcoes_reutilizadas import helper_busca_disciplinas
+
 
 class CapturaPresencaImagem(App):
     def build(self):
@@ -24,34 +28,49 @@ class CapturaPresencaImagem(App):
         try:
             with open("Configuracoes/config.txt", "r") as config_file:
                 config = json.load(config_file)
-                # select_cam = config.get("select_cam", "")
                 select_matricula = config.get("select_matricula", "")
                 select_disciplina = config.get("select_disciplina", "")
                 select_curso = config.get("select_curso", "")
         except FileNotFoundError:
-            pass
+            select_disciplina = ""
+            select_curso = ""
 
         left_column = BoxLayout(orientation='vertical', spacing=10)
         left_column.add_widget(self.image)
-        self.file_chooser = FileChooserListView(path=str("CapturasDeTurma")) # Pasta que vai abrir como default
+        self.file_chooser = FileChooserListView(path=str("CapturasDeTurma"))
         self.file_chooser.bind(on_submit=self.load_image)
         left_column.add_widget(self.file_chooser)
 
-        # Adição dos widgets na tela
         self.label_date = Label(text='Data:')
         self.label_text_input1 = Label(text='Curso:')
         self.label_text_input2 = Label(text='Disciplina:')
         data_de_hoje = datetime.datetime.now().strftime('%Y-%m-%d')
         self.date_input = TextInput(hint_text='Data', text=data_de_hoje)
         self.text_input1 = TextInput(hint_text='Curso', text=select_curso)
-        self.text_input2 = TextInput(hint_text='Disciplina', text=select_disciplina)
+
+        self.text_input2 = TextInput(size_hint_y=None, height='48dp', hint_text='Selecione a Disciplina',
+                                     readonly=True)
+
+        helper = helper_busca_disciplinas()
+        lista_disciplinas = helper.lista_de_disciplinas_cadastradas()
+        dropdown = DropDown()
+
+        for disciplina in lista_disciplinas:
+            btn = Button(text=disciplina, size_hint_y=None, height=44)
+            btn.bind(on_release=lambda btn: (setattr(self, 'select_disciplina', btn.text),
+                                             setattr(self.text_input2, 'text', btn.text),
+                                             dropdown.dismiss()))
+            dropdown.add_widget(btn)
+
+        self.text_input2.bind(
+            on_touch_down=lambda instance, touch: dropdown.open(self.text_input2) if instance.collide_point(
+                *touch.pos) else None)
 
         generate_button = Button(text='Gerar Presença')
         close_button = Button(text='Fechar')
         generate_button.bind(on_press=self.generate_presence)
         close_button.bind(on_press=self.close_popup)
 
-        # Adição dos widgets na coluna da direita
         right_column = BoxLayout(orientation='vertical', spacing=10)
         right_column.add_widget(self.label_date)
         right_column.add_widget(self.date_input)
@@ -69,92 +88,104 @@ class CapturaPresencaImagem(App):
         return layout
 
     def load_image(self, instance, selection, *args):
-        value = selection[0]
-        if os.path.isfile(value) and value.lower().endswith(('.png', '.jpg', '.jpeg')):
-            self.image.source = value
-        else:
-            self.show_error_popup("Selecione um arquivo de imagem válido.")
+        if selection:
+            value = selection[0]
+            if os.path.isfile(value) and value.lower().endswith(('.png', '.jpg', '.jpeg')):
+                self.image.source = value
+            else:
+                self.show_error_popup("Selecione um arquivo de imagem válido.")
 
     def generate_presence(self, instance):
         data = self.date_input.text
         curso = self.text_input1.text
         disciplina = self.text_input2.text
-        filename = f"{curso}_{disciplina}_{data}.xlsx"
 
+        if not disciplina:
+            self.show_error_popup("Por favor, selecione uma disciplina antes de gerar a presença.")
+            return
+
+        if "selecione_imagem.png" in self.image.source:
+            self.show_error_popup("Por favor, selecione uma imagem antes de gerar a presença.")
+            return
+
+        people_dir = os.path.join("Alunos", disciplina)
+
+        known_faces, known_names = self.load_known_faces(people_dir)
+
+        if not known_faces:
+            self.show_error_popup("Nenhum rosto conhecido foi carregado. Verifique as imagens dos alunos.")
+            return
+
+        all_people = [os.path.splitext(filename)[0] for filename in os.listdir(people_dir)
+                      if filename.endswith(('.png', '.jpg', '.jpeg'))]
+
+        recognized_people = self.get_pessoas_presentes(self.image.source, known_faces, known_names)
+
+        filename = f"{curso}_{disciplina}_{data}.xlsx"
         workbook = Workbook()
         sheet = workbook.active
-
-        # Criação das Colunas
         sheet.cell(row=1, column=1, value="Aluno")
         sheet.cell(row=1, column=2, value="Matricula")
         sheet.cell(row=1, column=3, value="Status")
 
-        # Diretório de imagens das pessoas registradas
-        people_dir = "Alunos"
-
-        # Carregar imagens das pessoas registradas
-        known_faces, known_names = self.load_known_faces(people_dir)
-
-        # Define todas as pessoas como "AUSENTE" inicialmente para preenchimento
-        all_people = [os.path.splitext(filename)[0] for filename in os.listdir(people_dir)]
         row_index = 2
 
-        # Faces reconhecidas na imagem selecionada
-        recognized_people = self.get_pessoas_presentes(self.image.source, known_faces, known_names)
-
         for person in all_people:
-            person = person.replace(".png", "")
-            nome_matricula = person.split("_")
-            nome = nome_matricula[0]
-            matricula = nome_matricula[1]
+            person = person.replace(".png", "").replace(".jpg", "").replace(".jpeg", "")
 
-            # Verificar se a pessoa está presente e definir o valor correspondente
-            if person in recognized_people:
-                status = "PRESENTE"
-            else:
-                status = "AUSENTE"
+            try:
+                _, nome, matricula = person.split("_", 2)
+            except ValueError:
+                print(f"Formato inválido para o arquivo: {person}")
+                continue
+
+            status = "PRESENTE" if person in recognized_people else "AUSENTE"
 
             sheet.cell(row=row_index, column=1, value=nome)
             sheet.cell(row=row_index, column=2, value=matricula)
             sheet.cell(row=row_index, column=3, value=status)
             row_index += 1
 
-        # Salvando o arquivo Excel no diretório designado
         app_root_dir = os.path.dirname(os.path.abspath(__file__))
         save_dir = os.path.join(app_root_dir, "PresencasCapturadas")
-        os.makedirs(save_dir, exist_ok=True)  # Crie o diretório se ele não existir
+        os.makedirs(save_dir, exist_ok=True)
 
         file_path = os.path.join(save_dir, filename)
         workbook.save(file_path)
 
-        self.show_info_popup(f'Arquivo de presença "{filename}" \nfoi gerado com sucesso na Pasta.')
+        self.show_info_popup(f'Arquivo de presença "{filename}" \nfoi gerado com sucesso na pasta.')
 
     def load_known_faces(self, people_dir):
         known_faces = []
         known_names = []
 
+        if not os.path.exists(people_dir):
+            self.show_error_popup(f"A pasta '{people_dir}' não existe.")
+            return [], []
+
         for person_filename in os.listdir(people_dir):
             person_path = os.path.join(people_dir, person_filename)
             if person_filename.endswith(('.png', '.jpg', '.jpeg')):
                 image = face_recognition.load_image_file(person_path)
-                encoding = face_recognition.face_encodings(image)[0]  # Assumindo um único rosto na imagem
-                known_faces.append(encoding)
-                known_names.append(os.path.splitext(person_filename)[0])
+                encodings = face_recognition.face_encodings(image)
+
+                if len(encodings) > 0:
+                    known_faces.append(encodings[0])
+                    person_name = os.path.splitext(person_filename)[0]
+                    known_names.append(person_name)
+                else:
+                    print(f"Nenhum rosto detectado em: {person_filename}")
 
         return known_faces, known_names
 
     def get_pessoas_presentes(self, image_path, known_faces, known_names):
-        # Buscando a imagem selecionada
         image = face_recognition.load_image_file(image_path)
-
-        # Busca de rostos na imagem
         face_locations = face_recognition.face_locations(image)
         face_encodings = face_recognition.face_encodings(image, face_locations)
 
         recognized_people = []
 
         for face_encoding in face_encodings:
-            # Comparação de face com as imagens das pessoas registradas
             matches = face_recognition.compare_faces(known_faces, face_encoding)
             if any(matches):
                 face_index = matches.index(True)
